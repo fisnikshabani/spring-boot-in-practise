1c2c8967907c7f4ee2066b5a65088f31056e072e
Fisnik Shabani <fisnik.shabani@gmail.com>
2023-10-15 01:37:07 +0200

    Chapter 3 examples

---
+++ chapter3/configuring-relational-database/src/main/java/com/example/configuringrelationaldatabase/ConfiguringRelationalDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.configuringrelationaldatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfiguringRelationalDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfiguringRelationalDatabaseApplication.class, args);
+    }
+
+}
---
+++ chapter3/configuring-relational-database/src/test/java/com/example/configuringrelationaldatabase/ConfiguringRelationalDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configuringrelationaldatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfiguringRelationalDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
