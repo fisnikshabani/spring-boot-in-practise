a5f75ca9f64fc88e73c6eabe430a086c7d4b0f0a
Fisnik Shabani <fisnik.shabani@gmail.com>
2023-10-16 15:40:10 +0200

    Chapter 3 - Managing domain objects in a relational database with Spring Data JPA

---
+++ chapter3/manage-domain-objects-with-jpa/src/main/java/com/example/managedomainobjectswithjpa/ManageDomainObjectsWithJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.managedomainobjectswithjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageDomainObjectsWithJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageDomainObjectsWithJpaApplication.class, args);
+    }
+
+}
---
+++ chapter3/manage-domain-objects-with-jpa/src/test/java/com/example/managedomainobjectswithjpa/ManageDomainObjectsWithJpaApplicationTests.java
@@ -0,0 +1,60 @@
+package com.example.managedomainobjectswithjpa;
+
+import model.Course;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import repository.CourseRepository;
+
+@SpringBootTest
+class ManageDomainObjectsWithJpaApplicationTests {
+
+    @Autowired
+   private CourseRepository courseRepository;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void givenCreateCourseWhenLoadTheCourseThenExpectSameCourse() {
+
+        Course course = new Course("Rapid Spring Boot Application Development", "Spring", 4,
+                "'Spring Boot gives all the power of the Spring Framework without all of the complexities");
+        Course savedCourse = courseRepository.save(course);
+
+        Assertions.assertThat(courseRepository.findById(savedCourse.getId()).get())
+                .isEqualTo(course);
+    }
+
+    @Test
+    void givenUpdateCourseWhenLoadTheCourseThenExpectUpdatedCourse() {
+
+        Course course = new Course("Rapid Spring Boot Application Development", "Spring", 4,
+                "'Spring Boot gives all the power of the Spring Framework without all of the complexities");
+        courseRepository.save(course);
+        course.setRating(5);
+
+        Course savedCourses = courseRepository.save(course);
+
+        Assertions.assertThat(courseRepository.findById(savedCourses.getId()).get().getRating())
+                .isEqualTo(5);
+    }
+
+    @Test
+    void givenDeleteCourseWhenLoadTheCourseThenExpectNoCourse() {
+
+        Course course = new Course("Rapid Spring Boot Application Development", "Spring", 4,
+                "'Spring Boot gives all the power of the Spring Framework without all of the complexities");
+
+        Course savedCourses = courseRepository.save(course);
+
+        Assertions.assertThat(courseRepository.findById(savedCourses.getId()).get()).isEqualTo(course);
+
+        courseRepository.delete(course);
+
+        Assertions.assertThat(courseRepository.findById(savedCourses.getId()).isPresent()).isFalse();
+    }
+}
