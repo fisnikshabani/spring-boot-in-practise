e589a3cb4a19c27dc8536b94743f218f85fa1e24
Fisnik Shabani <fisnik.shabani@gmail.com>
2023-11-04 00:07:52 +0100

    Chapter 3 - Using a named query to manage domain objects in a relational database with Spring Data JPA

---
+++ chapter3/named-query-method/src/main/java/com/example/namedquerymethod/model/Course.java
@@ -0,0 +1,96 @@
+package com.example.namedquerymethod.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table (name = "COURSES")
+@NamedQuery(name = "Course.findAllByCategoryAndRating",
+        query = "select c from Course c where c.category=?1 and c.rating=?2")
+public class Course {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    private String category;
+
+    private int rating;
+
+    private String description;
+
+    public Course() {
+    }
+
+    public Course(String name, String category, int rating, String description) {
+        this.name = name;
+        this.category = category;
+        this.rating = rating;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Course)) return false;
+        Course course = (Course) o;
+        return Objects.equals(name, course.name) && Objects.equals(category, course.category);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, category);
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", rating=" + rating +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
